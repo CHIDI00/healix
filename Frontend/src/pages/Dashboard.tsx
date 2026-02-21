efe2babd237b73d1fc42aa6bf2ddb53db42cd034
Chidi Mathaias <chidimathayas@gmail.com>
2026-02-21 20:06:15 +0100

    add OrbAnimation component and enhance AI Hero Button styling

--- Frontend/src/pages/Dashboard.tsx
+++ Frontend/src/pages/Dashboard.tsx
@@ -12,6 +12,7 @@ import {
 } from "lucide-react";
 import AIChatModal from "@/components/models/AIChatModal";
 import VitalsModal from "@/components/models/VitalsModal";
+import Orb from "@/components/ui/OrbAnimation";
 
 const quickCards = [
   {
@@ -144,7 +145,7 @@ const Dashboard = ({ onLogout }: DashboardProps) => {
       <div className=" flex flex-1 items-center justify-center">
         <motion.button
           onClick={() => setChatOpen(true)}
-          className="relative group flex h-56 w-56 p-0 flex-col items-center justify-center rounded-full border border-indigo-100 bg-white"
+          className="relative group flex h-56 w-56 flex-col items-center justify-center rounded-full "
           style={{
             boxShadow: spikeActive
               ? "0 0 60px rgba(239,68,68,0.4)"
@@ -176,6 +177,16 @@ const Dashboard = ({ onLogout }: DashboardProps) => {
           >
             Helix
           </span>
+
+          <div className="w-full h-full absolute inset-0 flex items-center justify-center overflow-hidden rounded-full">
+            <Orb
+              hoverIntensity={0.2}
+              rotateOnHover
+              hue={0}
+              forceHoverState={false}
+              backgroundColor="#000000"
+            />
+          </div>
         </motion.button>
       </div>
 
