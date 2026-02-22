eb7eafedf65e1d86cafc5f7580efc31fca27cb95
EnejiOhieku <enejiohieku@gmail.com>
2026-02-22 04:19:43 +0100

    added tables to the admin site

--- backend/healix_server/healix_server/admin.py
+++ backend/healix_server/healix_server/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import VitalSigns, PhysicalData, FitnessData, Nutrition, ReproductiveHealth, Sleep
+from .models import VitalSigns, PhysicalData, FitnessData, Nutrition, ReproductiveHealth, Sleep, EmergencyContact
 
 admin.site.register(VitalSigns)
+admin.site.register(PhysicalData)
+admin.site.register(FitnessData)
+admin.site.register(Nutrition)
+admin.site.register(ReproductiveHealth)
+admin.site.register(Sleep)
+admin.site.register(EmergencyContact)
+
+# Register your models here.
